104bfdf55c8d2879377a106d4921821f608f170d
APex <37938428+APexBlockchainDevelopments@users.noreply.github.com>
2024-01-11 09:29:12 -0700

    Updated tets

--- src/ManualToken.sol
+++ src/ManualToken.sol
@@ -129,23 +129,23 @@ contract ManualToken{
      * @param _spender The address authorized to spend
      * @param _value the max amount they can spend
      * @param _extraData some extra information to send to the approved contract
-     */
-    function approveAndCall(
-        address _spender,
-        uint256 _value,
-        bytes memory _extraData
-    ) public returns (bool success) {
-        tokenRecipient spender = tokenRecipient(_spender);
-        if (approve(_spender, _value)) {
-            spender.receiveApproval(
-                msg.sender,
-                _value,
-                address(this),
-                _extraData
-            );
-            return true;
-        }
-    }
+    //  */
+    // function approveAndCall(
+    //     address _spender,
+    //     uint256 _value,
+    //     bytes memory _extraData
+    // ) public returns (bool success) {
+    //     tokenRecipient spender = tokenRecipient(_spender);
+    //     if (approve(_spender, _value)) {
+    //         spender.receiveApproval(
+    //             msg.sender,
+    //             _value,
+    //             address(this),
+    //             _extraData
+    //         );
+    //         return true;
+    //     }
+    // }
 
     /**
      * Destroy tokens
--- src/OurToken.sol
+++ src/OurToken.sol
@@ -2,6 +2,10 @@
 
 pragma solidity ^0.8.19;
 
-contract OurToken{
-    
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract OurToken is ERC20{
+    constructor(uint256 initialSupply)ERC20("OurToken", "OT"){
+        _mint(msg.sender, initialSupply);
+    }
 }
